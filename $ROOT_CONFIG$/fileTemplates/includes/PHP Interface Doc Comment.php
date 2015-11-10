4b69dc240dfcadf29e0ed9114e8e37fb4b057c07
Administrator <Administrator@LSTONE-PC>
2015-11-10 12:50:26 +0800

    PS-10.0 <Administrator@LSTONE-PC Create Customized.xml, ide.general.xml, Default.xml, PHP Fluent Setter Method.php, project.default.xml, diff.xml, PHP Getter Method.php, other.xml, file.template.settings.xml, PHP Interface Doc Comment.php, ui.lnf.xml, php.xml, keymap.xml, code.style.schemes.xml, colors.scheme.xml, PHP Constructor.php, PHP Setter Method.php, Customized.icls, PHP Class Doc Comment.php, laf.xml, web-browsers.xml, PHP File Header.php, proxy.settings.xml, editor.codeinsight.xml, editor.xml, debugger.xml

---
+++ $ROOT_CONFIG$/fileTemplates/includes/PHP Interface Doc Comment.php
@@ -0,0 +1,12 @@
+#set($VERSION='1.0.1')
+/**
+ * Interface ${NAME}.
+ * 
+ * --------------------------------------------------------
+ * @author        Lei Lee <web.developer.network@gmail.com>
+ * @version       ${VERSION}
+ * @copyright (c) 2015-2016, Lei Lee
+#if (${NAMESPACE}) * @package ${NAMESPACE}
+#end
+ * --------------------------------------------------------
+ */
