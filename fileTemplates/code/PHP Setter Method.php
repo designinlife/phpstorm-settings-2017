0bc7b2f13c383588f819c965f0ca2feaa8ee1096
Administrator <Administrator@LSTONE-PC>
2015-11-10 17:54:19 +0800

    PhpStorm <Administrator@LSTONE-PC Get rid of $ROOT_CONFIG$ and $APP_CONFIG

--- fileTemplates/code/PHP Setter Method.php
+++ fileTemplates/code/PHP Setter Method.php
@@ -2,8 +2,13 @@
  * @param ${TYPE_HINT} $${PARAM_NAME}
  * @return ${CLASS_NAME}
  */
-function set${NAME}($${PARAM_NAME})
+${STATIC} function set${NAME}($${PARAM_NAME})
 {
+#if (${STATIC} == "static")
+    self::$${FIELD_NAME} = $${PARAM_NAME};
+#else
     $this->${FIELD_NAME} = $${PARAM_NAME};
+#end
+
     return $this;
 }
